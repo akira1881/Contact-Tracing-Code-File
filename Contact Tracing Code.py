c6771c5d6c1413437026ef09d5ad5d16a1703c35
Charina Vallecera <charinavallecera08@gmail.com>
2022-11-12 22:36:48 +0800

    Add and Search Intro Codes

--- Contact Tracing Code.py
+++ Contact Tracing Code.py
@@ -5,21 +5,27 @@
 print("Contact Tracing Menu")
 print()
 print(">>>>>>> MENU <<<<<<<")
-print(" 1.) ADD    "
-      " 2.) SEARCH " 
-      " 3.) EXIT   ")
+print(" 1.) ADD    ")
+print(" 2.) SEARCH ")
+print(" 3.) EXIT   ")
 
 file = []
 
 
 def add():
-    print(">>>>>>> Add New <<<<<<<")
-    user_input = input("Fullname: ")
-    file.append(user_input)
-    user_age = input("Age: ")
-    file.append(user_age)
-    user_address = input("Address: ")
-    file.append(user_address)
-    user_number = input("Phone Number: ")
-    file.append(user_number)
-    print("SAVED!")
+    while True:
+        print(">>>>>>> Add New Contact <<<<<<<")
+        user_input = input("Fullname: ")
+        file.append(user_input)
+        user_age = input("Age: ")
+        file.append(user_age)
+        user_address = input("Address: ")
+        file.append(user_address)
+        user_number = input("Phone Number: ")
+        file.append(user_number)
+        print("SAVED!")
+
+def search():
+    while True:
+        print(">>>>>>> Search Contact <<<<<<<")
+        
