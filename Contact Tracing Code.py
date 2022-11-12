d9e3720b25b3360ed59be10346fb607321a24070
Charina Vallecera <charinavallecera08@gmail.com>
2022-11-13 03:27:38 +0800

    Initial Commit

--- Contact Tracing Code.py
+++ Contact Tracing Code.py
@@ -1,13 +1,21 @@
 # Contact Tracing Files
-# Prgrammed by: Charina C. Vallecera
+# Programmed by: Charina C. Vallecera
 # BSCOE 2-2
 
-file = {"Rasha Avery": "21 years old ----  Pasig City  ----  09563742834",
-        "Rhaenyra Edwards": "19 years old ---- Pasay City ---- 09275463249",
-        "Ronnie Karev": "22 years old ---- Quezon City ---- 09182745947"}
-print(file)
+file_info = {
+    'Rasha Avery': {'Age': 21, 'Address': 'Pasig City', 'Phone Number': '095656543845'},
+    'Mirabel Garcia': {'Age': 32, 'Address': 'Pasay City', 'Phone Number': '09875673424'},
+    'Julie Stevens': {'Age': 26, 'Address': 'Cebu City', 'Phone Number': '09315843258'},
+    'Cassie del Valle': {'Age': 18, 'Address': 'Bulacan', 'Phone Number': '09125349876'},
+    'Avelina Luna': {'Age': 43, 'Address': 'Maasin City', 'Phone Number': '09182322379384'},
+    'Jacob Potter': {'Age': 14, 'Address': 'Quezon City', 'Phone Number': '09188675742'},
+    'Luther King': {'Age': 61, 'Address': 'Cavite City', 'Phone Number': '09341287834'},
+    'Cris Alonzo': {'Age': 36, 'Address': 'Rizal', 'Phone Number': '09342217856'},
+    'Chandria Ramirez': {'Age': 25, 'Address': 'Laguna', 'Phone Number': '09518439487'}
+}
 
-def main_menu():
+
+def base():
     print(">>>>>>> CONTACT TRACING <<<<<<<")
     print()
     print(">>>>>>> MENU <<<<<<<")
@@ -18,74 +26,45 @@ def main_menu():
     print()
 
 
-def add():
-    while True:
-        print(">>>>>>> Add New Contact <<<<<<<")
-        print()
-        user_input = input("Fullname: ")
-        user_age = input("Age: ")
-        user_address = input("Address: ")
-        user_number = input("Phone Number: ")
-        whole_info = f"\t{user_input: <20}{user_age: <20}{user_address: <60}{user_number: <15}"
-        file.update(whole_info)
-        print()
-        print("SAVED!")
-        print()
+def baseline():
+    base()
+    final_option = int(input("What would you like to do?:"))
+    if final_option == 1:
+        while True:
+            print(">>>>>>> Add New Contact <<<<<<<")
+            print()
+            user_input = str(input("Fullname: "))
+            user_age = str(input("Age: "))
+            user_address = str(input("Address: "))
+            user_number = str(input("Phone Number: "))
+            align = user_age + user_address + user_number
+            file.update(align)
+            print()
+            print("SAVED!")
+            print()
 
-        chance = input("Would you like to add another contact? (yes/no) ")
-        if chance.casefold() == "yes":
-            add()
-        elif chance.casefold() == "no":
-            coding()
+    elif final_option == 2:
+        while True:
+            print(">>>>>>> Search Contact <<<<<<<")
+            print()
+            search_input = input("Type the Fullname: ").casefold()
+            print(f"\t\t\t{'Fullname': <5s}{'Age': <5s}{'Address': <5s}{'Phone Number': <5s}")
+            file.get(search_input)
+            print(">>>>>>> Contact Found! <<<<<<<")
+            print()
 
+    elif final_option == 3:
+        while True:
+            print(">>>>>>> Exit <<<<<<<")
+            print()
+            print("Thank you for using this program!!!")
+            print("Bye!")
+            print()
 
-def search():
-    while True:
-        print(">>>>>>> Search Contact <<<<<<<")
-        print()
-        search_input = input("Type the Fullname")
-        file.get(search_input)
-        print()
-        print(">>>>>>> Contact Found! <<<<<<<")
-        print()
-
-        chanceAgain = input("Would you like to search another contact? (yes/no) ")
-        if chanceAgain.casefold() == "yes":
-            add()
-        elif chanceAgain.casefold() == "no":
-            coding()
-
-
-def full_tab():
-    while True:
-        print(">>>>>>> List Format <<<<<<<")
-        print("")
-
-
-def exits():
-    while True:
-        print(">>>>>>> Exit <<<<<<<")
-        print()
-        print("Thank you for using this program!!!")
-        print("Bye!")
-        print()
-        break
-
-
-def coding():
-    while True:
-        main_menu()
-        final_option = int(input("Choose the number of your choice: "))
-        if final_option == "1":
-            add()
-        elif final_option == "2":
-            search()
-        elif final_option == "3":
-            exits()
-        else:
-            print("Invalid input, please try again!")
-            coding()
+    else:
+        print("Invalid Input!")
+        baseline()
 
 
 while True:
-    coding()
+    baseline()
