5d417fd8681cf871fca6d168d9ba2260ad44f94e
Charina Vallecera <charinavallecera08@gmail.com>
2022-11-13 04:03:25 +0800

    another initial commit

--- Contact Tracing Code.py
+++ Contact Tracing Code.py
@@ -11,7 +11,8 @@ main_file = {
     'Jacob Potter': {'Age': 14, 'Address': 'Quezon City', 'Phone Number': '09188675742'},
     'Luther King': {'Age': 61, 'Address': 'Cavite City', 'Phone Number': '09341287834'},
     'Cris Alonzo': {'Age': 36, 'Address': 'Rizal', 'Phone Number': '09342217856'},
-    'Chandria Ramirez': {'Age': 25, 'Address': 'Laguna', 'Phone Number': '09518439487'}
+    'Chandria Ramirez': {'Age': 25, 'Address': 'Laguna', 'Phone Number': '09518439487'},
+    'Emma Dawson': {'Age': 63, 'Address': 'Bulacan', 'Phone Number': '09418675428'}
 }
 
 
@@ -38,24 +39,38 @@ def baseline():
             three = str(input("Address: "))
             four = str(input("Phone Number: "))
             align = {'Age': two, 'Address': three, 'Phone': four}
-            main_file = {one:align}
+            main_file[one] = align
             print()
             print("SAVED!")
             print()
+
+            again = (input("Go back to main?: (yes or no) ")).casefold()
+            if again == 'yes':
+                baseline()
+            elif again == 'no':
+                continue
 
     elif final_option == 2:
         while True:
             print(">>>>>>> Search Contact <<<<<<<")
             print()
             search_input = input("Type the Fullname: ").casefold()
-            print(f"\t\t\t{'Fullname': <5s}{'Age': <5s}{'Address': <5s}{'Phone Number': <5s}")
-            file_info.get(search_input)
+            print()
+            print(f"\t\t\t{'Name': <3s}",search_input,(main_file[search_input]['Age']),
+                  (main_file[search_input]['Address']),(main_file[search_input]['Phone']))
+            print()
             print(">>>>>>> Contact Found! <<<<<<<")
             print()
 
+            again = (input("Go back to main?: (yes or no) ")).casefold()
+            if again == 'yes':
+                baseline()
+            elif again == 'no':
+                continue
+
     elif final_option == 3:
         while True:
-            print(">>>>>>> Exit <<<<<<<")
+            print(">>>>>>>>>>> Exit <<<<<<<<<<<")
             print()
             print("Thank you for using this program!!!")
             print("Bye!")
